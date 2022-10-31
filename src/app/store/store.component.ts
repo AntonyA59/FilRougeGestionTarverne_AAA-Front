5d29118fc74d5b909867ef894a75f61ac4c7e746
PANO-PC\HEYMA <heyman591@hotmail.com>
2022-10-31 17:40:25 +0100

    store -> gestion du panier avec quantités des ingrédients

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -46,6 +46,13 @@ export class StoreComponent implements OnInit {
       idSubCategory: 6,
     },
     {
+      id: 64,
+      name: 'Bière',
+      level: 1,
+      buyingPrice: 1,
+      idSubCategory: 64,
+    },
+    {
       id: 14,
       name: 'Cuisse de Boeuf',
       level: 1,
@@ -53,8 +60,64 @@ export class StoreComponent implements OnInit {
       idSubCategory: 6,
     },
   ];
+  inventory: Ingredient[] = [
+    {
+      id: 64,
+      name: 'Bière',
+      level: 1,
+      buyingPrice: 1,
+      idSubCategory: 64,
+    },
+  ];
+  cartSelling: Ingredient[] = [];
+  cartBuying: Ingredient[] = [];
 
   constructor() {}
 
   ngOnInit(): void {}
+  addIngredientToBuying(id: number) {
+    let ingredient: Ingredient | undefined;
+    let ingredientAlreadyExist: Ingredient | undefined;
+    ingredient = this.ingredients.find((element) => element.id == id);
+    ingredientAlreadyExist = this.cartBuying.find(
+      (element) => element.id == id
+    );
+    if (ingredientAlreadyExist === undefined && ingredient != undefined) {
+      ingredient.count = 1;
+      this.cartBuying.push(ingredient);
+    } else {
+      if (ingredient != undefined && ingredient.count != undefined) {
+        ingredient.count++;
+      }
+    }
+  }
+  addIngredientToSelling(id: number) {
+    let ingredient: Ingredient;
+    ingredient = this.ingredients.find((element) => element.id == id)!;
+    for (let index = 0; index < this.inventory.length; index++) {
+      if (this.inventory[index].id == id) {
+        this.cartSelling.push(this.inventory[index]);
+        this.inventory.splice(index, 1);
+        break;
+      }
+    }
+  }
+
+  removeIngredientToBuying(id: number) {
+    for (let index = 0; index < this.cartBuying.length; index++) {
+      if (this.cartBuying[index].id == id) {
+        this.cartBuying.splice(index, 1);
+        break;
+      }
+    }
+  }
+  removeIngredientToSelling(id: number) {
+    for (let index = 0; index < this.cartSelling.length; index++) {
+      if (this.cartSelling[index].id == id) {
+        this.inventory.push(this.cartSelling[index]);
+        this.cartSelling.splice(index, 1);
+        break;
+      }
+    }
+  }
 }
