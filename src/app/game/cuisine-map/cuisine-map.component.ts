feacdf8ca9d4de997d44e71f56febc94880c4b64
PANO-PC\HEYMA <heyman591@hotmail.com>
2022-11-05 17:20:30 +0100

    Cuisine => selectioner d'une recette / bouton enabled en fonction de l'inventaire

--- src/app/game/cuisine-map/cuisine-map.component.ts
+++ src/app/game/cuisine-map/cuisine-map.component.ts
@@ -8,6 +8,64 @@ import { Recipe } from 'src/app/interfaces/recipe';
   styleUrls: ['./cuisine-map.component.css'],
 })
 export class CuisineMapComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    {
+      id: 2,
+      name: 'Gruit',
+      level: 1,
+      buyingPrice: 2,
+      idSubCategory: 1,
+    },
+    {
+      id: 6,
+      name: 'Pomme Verte',
+      level: 1,
+      buyingPrice: 1,
+      idSubCategory: 9,
+    },
+    {
+      id: 8,
+      name: 'Boulettes de Boeuf',
+      level: 1,
+      buyingPrice: 1,
+      idSubCategory: 6,
+    },
+    {
+      id: 10,
+      name: 'Omelette aux Herbes',
+      level: 1,
+      buyingPrice: 1,
+      idSubCategory: 6,
+    },
+    {
+      id: 13,
+      name: 'Blanc de Poulet',
+      level: 1,
+      buyingPrice: 1,
+      idSubCategory: 6,
+    },
+    {
+      id: 64,
+      name: 'Bière',
+      level: 1,
+      buyingPrice: 1,
+      idSubCategory: 64,
+    },
+    {
+      id: 14,
+      name: 'Cuisse de Boeuf',
+      level: 1,
+      buyingPrice: 1,
+      idSubCategory: 6,
+    },
+    {
+      id: 94,
+      name: 'Pain',
+      level: 1,
+      buyingPrice: 3,
+      idSubCategory: 6,
+    },
+  ];
   inventory: Ingredient[] = [
     {
       id: 64,
@@ -19,11 +77,19 @@ export class CuisineMapComponent implements OnInit {
     },
     {
       id: 94,
-      name: 'Cuisse de Boeuf',
+      name: 'Pain',
       level: 1,
       buyingPrice: 3,
       idSubCategory: 6,
       count: 2,
+    },
+    {
+      id: 14,
+      name: 'Cuisse de Boeuf',
+      level: 1,
+      buyingPrice: 1,
+      idSubCategory: 6,
+      count: 1,
     },
   ];
   recipes: Recipe[] = [
@@ -37,7 +103,7 @@ export class CuisineMapComponent implements OnInit {
       peremptionDate: '',
       expGiven: 0,
       idSubCategory: 1,
-      tabIngredientsForRecipe: [1],
+      tabIngredientsForRecipe: [94],
     },
     {
       id: 2,
@@ -49,13 +115,53 @@ export class CuisineMapComponent implements OnInit {
       peremptionDate: '',
       expGiven: 0,
       idSubCategory: 1,
-      tabIngredientsForRecipe: [1],
+      tabIngredientsForRecipe: [64, 10, 13, 14],
     },
   ];
-  IngredientsRecipe: Recipe[] = [];
+  ingredientsRecipe: Ingredient[] = [];
   numberNothing: number[] = [1, 1, 1, 1, 1];
+  recipeSelected = {} as Recipe;
+  ingredientsQuantityAvailable: number[] = [];
+  textdark = 'text-dark';
+  textred = 'text-danger';
+  recipeReady = false;
 
   constructor() {}
 
   ngOnInit(): void {}
+  selectRecipe(index: number) {
+    let ingredient: Ingredient | undefined;
+    let nbIngredients = 0;
+
+    if (this.recipeSelected.id != this.recipes[index].id) {
+      this.ingredientsRecipe = [];
+      this.numberNothing = [];
+      this.ingredientsQuantityAvailable = [];
+      this.recipeSelected = this.recipes[index];
+      this.recipeReady = true;
+
+      this.recipeSelected.tabIngredientsForRecipe.forEach((idIngredient) => {
+        ingredient = this.ingredients.find(
+          (element) => element.id == idIngredient
+        );
+        if (ingredient != undefined) {
+          this.ingredientsRecipe.push(ingredient);
+        }
+        ingredient = this.inventory.find(
+          (element) => element.id == idIngredient
+        );
+        if (ingredient != undefined) {
+          this.ingredientsQuantityAvailable.push(ingredient.count!);
+        } else {
+          this.ingredientsQuantityAvailable.push(0);
+          this.recipeReady = false;
+        }
+        nbIngredients++;
+      });
+
+      for (let index = 0; index < 5 - nbIngredients; index++) {
+        this.numberNothing.push(1);
+      }
+    }
+  }
 }
