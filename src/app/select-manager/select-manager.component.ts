c33a46c9aab1b23282ab19c3e60625d9f604cdaa
ASUS_ALEX\33631 <a.six109@laposte.net>
2022-11-10 11:09:23 +0100

    refresh function with idManager in the session store OK

--- src/app/select-manager/select-manager.component.ts
+++ src/app/select-manager/select-manager.component.ts
@@ -11,7 +11,6 @@ import { ManagerService } from '../services/manager/manager.service';
 export class SelectManagerComponent implements OnInit {
   managers: ManagerModel[] = [];
   constructor(
-    private loadManagerService: LoadManagerService,
     private managerService: ManagerService
   ) {}
 
@@ -22,6 +21,6 @@ export class SelectManagerComponent implements OnInit {
   }
 
   selectManager(idManager: number) {
-    this.loadManagerService.loadManager(idManager);
+    sessionStorage.setItem("idManager",idManager.toString());
   }
 }
