a3f57856467b6b1501c954a63408767576ded6f4
Antony <antony.alsberghe@hotmail.fr>
2022-11-14 09:24:05 +0100

    modif interface customer

--- src/app/interfaces/customer.ts
+++ src/app/interfaces/customer.ts
@@ -18,26 +18,6 @@ export interface CustomerModel {
   name?: string;
   numImg?: number;
 }
-export interface Customer {
-  id: number;
-  purseOfGold: number;
-  happiness: number;
-  hunger: number;
-  thirst: number;
-  nauseaLevel: number;
-  alcoholLevel: number;
-  toilet: number;
-  timeInTavern: string;
-  nauseaTolerance: number;
-  alcoholTolerance: number;
-  gender: number;
-  expGiven: number;
-  idTableRest?: number;
-  consommationStart?: string;
-  commandList?: number[];
-  name?: string;
-  numImg?: number;
-}
 
 export const tabName: string[] = [
   'Isaïe ',
